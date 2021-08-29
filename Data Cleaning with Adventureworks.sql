cbbd4aea3d23d6b520b2752d63bc6c354c313079
Jason Paulose <87837231+jason-paulose@users.noreply.github.com>
2021-08-29 17:57:27 -0400

    Add files via upload

--- Data Cleaning with Adventureworks.sql
+++ Data Cleaning with Adventureworks.sql
@@ -1,8 +1,12 @@
-   --Update the table with the proper date format
-UPDATE AdWorks..DimEmployee
-SET HireDate = FORMAT(HireDate, 'd','us'),
-BirthDate = FORMAT(BirthDate, 'd','us'),
-StartDate = FORMAT(StartDate, 'd','us')
+ SELECT *
+ FROM AdWorks..DimEmployee
+ 
+ --Standardize dates upon retrieval
+ SELECT FORMAT(HireDate, 'MMM dd yyyy') AS ConvertedHireDate,
+ FORMAT(BirthDate, 'MMM dd yyyy') AS ConvertedBirthDate,
+ FORMAT(StartDate, 'MMM dd yyyy') AS ConvertedStartDate,
+ FORMAT(EndDate, 'MMM dd yyyy') AS ConvertedEndDate
+ FROM AdWorks..DimEmployee
 
 -- handle null value in the status column; if NULL, assume they are terminated
 SELECT FirstName, LastName, ISNULL(Status, 'Terminated') AS TrueStatus
@@ -28,3 +32,32 @@ CASE
 END AS SalesTerritoryKey
 FROM AdWorks..DimEmployee e INNER JOIN AdWorks..DimSalesTerritory s ON e.SalesTerritoryKey = s.SalesTerritoryKey
 ORDER BY 2
+
+-- parse each employee username from the email and add it to the table
+SELECT SUBSTRING(EmailAddress, 1, CHARINDEX('@', EmailAddress)-1)
+FROM Adworks..DimEmployee
+
+ALTER TABLE Adworks..DimEmployee
+ADD UserName Nvarchar(255)
+
+UPDATE AdWorks..DimEmployee
+SET UserName = SUBSTRING(EmailAddress, 1, CHARINDEX('@', EmailAddress)-1)
+
+-- check to see if there are any null gender records that need to be addressed
+SELECT *
+FROM AdWorks..DimEmployee
+WHERE Gender = NULL
+
+-- all employees have a gender assigned, so now change M and F to Male and Female 
+ALTER TABLE Adworks..DimEmployee
+ALTER COLUMN Gender NVARCHAR(10)
+
+UPDATE AdWorks..DimEmployee
+SET Gender = CASE
+				WHEN Gender = 'F' THEN 'Female'
+				ELSE 'Male'
+			 END
+
+-- delete unused columns
+ALTER TABLE Adworks..DimEmployee
+DROP COLUMN ParentEmployeeNationalIDAlternateKey, MiddleName, SalesPersonFlag
